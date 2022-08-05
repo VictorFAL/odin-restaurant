70c8727ffb1266d24ee7392625f9275a4e838047
Victor | Harry <victorfalmeida12@gmail.com>
2022-08-05 14:07:12 -0300

    stylized page

--- src/index.js
+++ src/index.js
@@ -1,1 +1,31 @@
-console.log('TESSSST');
+import './style.css';
+// import Background from './barBackground.png';
+
+const content = document.getElementById("content");
+
+// TODO: Add tab buttons
+
+// TODO: Home page
+function home() {
+    const homeDiv = document.createElement('div');
+
+    homeDiv.innerHTML = `   <h1>The Abyss Bar</h1>
+                            <div>
+                                <h2>Open 24/7</h2>
+                            </div>
+                            <div>
+                                <h2>Location</h2>
+                                <p>Hoxxes IV, Space Rig 17</p>
+                            </div>`;
+
+    homeDiv.classList.add("info-div");
+    
+    return homeDiv;
+}
+
+// TODO: Menu page
+
+// TODO: Contact page
+
+// Test
+content.appendChild(home());
--- webpack.config.js
+++ webpack.config.js
@@ -6,10 +6,22 @@ module.exports = {
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
+    clean: true,
   },
-  // plugins: [
-  //   new HtmlWebpackPlugin({
-  //     title: 'Restaurant Page',
-  //   }),
-  // ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
 };
