c0e82cf4e7c1a22f9be562d178e964518cbcab36
Victor | Harry <victorfalmeida12@gmail.com>
2022-08-09 14:26:43 -0300

    tab interaction and menu ideas

--- src/index.js
+++ src/index.js
@@ -3,16 +3,38 @@ import home from './home';
 
 const content = document.getElementById("content");
 
-// TODO: Tab buttons
+// Tab buttons
+const homeTab = document.getElementById("home-tab");
+const menuTab = document.getElementById("menu-tab");
+const contactTab = document.getElementById("contact-tab");
 
 // Home page
 function homeRender() {
     content.innerHTML = home;
+    homeTab.style.borderBottom = "solid 1px rgba(14, 32, 32, 0.85)";
+    menuTab.style.borderBottom = "solid 1px #fef807";
+    contactTab.style.borderBottom = "solid 1px #fef807";
 }
 
 // TODO: Menu page
+function menuRender() {
+    // populate html
+    homeTab.style.borderBottom = "solid 1px #fef807";
+    menuTab.style.borderBottom = "solid 1px rgba(14, 32, 32, 0.85)";
+    contactTab.style.borderBottom = "solid 1px #fef807";
+}
 
 // TODO: Contact page
+function contactRender() {
+    // populate html
+    homeTab.style.borderBottom = "solid 1px #fef807";
+    menuTab.style.borderBottom = "solid 1px #fef807";
+    contactTab.style.borderBottom = "solid 1px rgba(14, 32, 32, 0.85)";
+}
 
-// Test
+homeTab.addEventListener('click', homeRender);
+menuTab.addEventListener('click', menuRender);
+contactTab.addEventListener('click', contactRender);
+
+// Render initial state
 homeRender();
---
+++ src/menu.js
@@ -0,0 +1,33 @@
+function menuSection() {
+    const beers = [
+        {
+            name: 'Oily Oaf',
+            imgUrl: './imgs/OilyOaf.png',
+            descr: 'A classic. While one of the lighter drinks available, the rich, buttery aftertaste coupled with the low price tag means the Oaf is here to stay.'
+        },
+        {
+            name: 'Glyphid Slammer',
+            imgUrl: './imgs/GlyphidSlammer.png',
+            descr: 'An adventurous mix of a cheap, powerful ale with an equally cheap, powerful energy drink. The result is almost, but not quite, entirely undrinkable. But it sure does put a spring in your step.'
+        },
+        {
+            name: 'Leaf Lover',
+            imgUrl: './imgs/LeafLover.png',
+            descr: "The Leaf Lover is on this chart entirely to please Management. It'll kill your buzz faster than a pay cut, and leave you with the same empty feeling in your gut. Still, it can be handy on Inspection Day - just don't let anyone know you had one."
+        },
+    ];
+    
+    const menuDiv = `<div class="info-div">
+                        <h1>Menu</h1>
+                        <div id="menu-wrapper">
+                        </div>
+                    </div>`;
+    
+    const table = document.getElementById("menu-wrapper");
+
+    b
+
+}
+
+
+export default menuSection;
